29a6bfa8689fbb01a6a7101fda48fab2424166c5
Isaac Yawson <isaacayawson1996@gmail.com>
2025-06-19 19:16:16 -0700

    Update settings.py

--- messaging/settings.py
+++ messaging/settings.py
@@ -4,5 +4,11 @@ CACHES = {
         'LOCATION': 'unique-snowflake',
         'rest_framework',
         'chats',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
